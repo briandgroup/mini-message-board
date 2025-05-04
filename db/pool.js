a42c2b4cff1c16fe8242a0d9eb942db5911011c2
brianooi <briansh1225@gmail.com>
2025-05-05 01:35:41 +0800

    add ssl for railway

--- db/pool.js
+++ db/pool.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false
+  ssl: {
+    rejectUnauthorized: false  // Required for Railway-managed PostgreSQL or self-signed certs
+  }
 });
 
 module.exports = pool;
